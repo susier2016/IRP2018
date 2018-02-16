a900972bf633ac83cf5fd075dfc44c45340b7a7e
Susie Renjith <>
2018-02-15 20:23:55 -0600

    First Commit

---
+++ iVoluntr/Login.swift
@@ -0,0 +1,26 @@
+//
+//  Login.swift
+//  iVoluntr
+//
+//  Created by Susie Renjith on 12/28/17.
+//  Copyright © 2017 Susie Renjith. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
---
+++ iVoluntr/SignUp.swift
@@ -0,0 +1,27 @@
+//
+//  SignUp.swift
+//  iVoluntr
+//
+//  Created by Susie Renjith on 12/28/17.
+//  Copyright © 2017 Susie Renjith. All rights reserved.
+//
+
+import UIKit
+
+class SignUp: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
+
